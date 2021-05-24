424bccad4c263dbc09839abc93e24f2ceddbcb09
swill <swilltec@gmail.com>
2021-05-24 13:32:23 +0000

    first commit

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+import config
+import routers
+import tasks
+
+
+def get_application():
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=["*"]
+    )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+
+    app.include_router(routers.router)
+
+    return app
+
+
+app = get_application()
