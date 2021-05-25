58e2a438b83abdb9f846016474efdd58cfb185db
swill <swilltec@gmail.com>
2021-05-25 05:16:02 +0000

    chore: modified project readme

--- backend/routers.py
+++ backend/routers.py
@@ -1,8 +1,7 @@
 import datetime
 import uuid
-from typing import List, Dict
 
-from fastapi import Depends, APIRouter
+from fastapi import APIRouter
 from models import Keys
 
 
@@ -10,7 +9,7 @@ from models import Keys
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=dict)
 async def home():
     await Keys.create(key=datetime.datetime.now(), uuid= uuid.uuid4().hex)
     keys = await Keys.all()
